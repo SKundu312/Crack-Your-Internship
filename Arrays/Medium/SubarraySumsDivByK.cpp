68eb64a0c675a81b2b2306ad1eba8cc77370b0ae
Shreya Kundu <81185880+SKundu312@users.noreply.github.com>
2022-06-22 11:05:14 +0530

    Update SubarraySumsDivByK.cpp

--- Arrays/Medium/SubarraySumsDivByK.cpp
+++ Arrays/Medium/SubarraySumsDivByK.cpp
@@ -15,4 +15,22 @@ public:
     }
 };
 
-//O(
+//O(N)
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        map<int,int>m;
+        int sum=0,rem=0,ans=0;
+        m.insert({0,1});
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            rem=sum%k;
+            if(rem<0)
+                rem+=k;
+            if(m.find(rem)!=m.end())
+                ans+=m[rem];
+            m[rem]++;
+        }
+        return ans;
+    }
+};
