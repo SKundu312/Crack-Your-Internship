0944399197c88fec4e3f51bc6a62fa34e0ad512e
Shreya Kundu <81185880+SKundu312@users.noreply.github.com>
2022-06-24 02:06:45 +0530

    Update DltMiddle.cpp

--- Linked_List/Medium/DltMiddle.cpp
+++ Linked_List/Medium/DltMiddle.cpp
@@ -33,3 +33,24 @@ public:
         return head;
     }
 };
+
+//O(N/2)
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if(!head->next) return NULL;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        ListNode* prev=NULL;
+        while(fast!=NULL && fast->next!=NULL){
+            prev=slow;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        if(prev)
+           prev->next=slow->next;
+        else
+            slow->next=NULL;
+        return head;
+    }
+};
