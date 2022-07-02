22a57d91e999f831fba0e40e9609f98ce3f4e802
Shreya Kundu <81185880+SKundu312@users.noreply.github.com>
2022-07-02 12:48:48 +0530

    Create ClimbingStairs.cpp

---
+++ Dynamic Programming/Easy/ClimbingStairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fn(int n, vector<int>& dp){
+        if(n==1) return 1;
+        if(n==2) return 2;
+        if(dp[n]!=-1) return dp[n];
+        else dp[n]=fn(n-1,dp)+fn(n-2,dp);
+        return dp[n];
+    }
+    int climbStairs(int n) {
+        vector<int> dp(46,-1);
+        return fn(n,dp);
+    }
+};
