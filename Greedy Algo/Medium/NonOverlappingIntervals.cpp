f3fa6fc58718de5cd52aae5411e16a7d68ff8357
Shreya Kundu <81185880+SKundu312@users.noreply.github.com>
2022-07-11 21:24:45 +0530

    Update NonOverlappingIntervals.cpp

--- Greedy Algo/Medium/NonOverlappingIntervals.cpp
+++ Greedy Algo/Medium/NonOverlappingIntervals.cpp
@@ -23,3 +23,24 @@ public:
     }
 };
 
+class Solution {
+public:
+    static bool cmp(vector<int>& v1, vector<int>& v2){
+        return v1[1]<v2[1];
+    }
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),cmp);
+        // for(int i=0;i<intervals.size();i++){
+        //     cout<<intervals[i][0]<<" "<<intervals[i][1]<<endl;
+        // }
+        int ans=1;
+        int prev=0;
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i][0]>=intervals[prev][1]){
+                ans++;
+                prev=i;
+            }
+        }
+        return intervals.size()-ans;
+    }
+};
