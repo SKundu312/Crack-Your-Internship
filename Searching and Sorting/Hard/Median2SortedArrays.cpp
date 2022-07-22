206bdd9b11f6b2dd7faf06ba760fbde69d8c27b8
Shreya Kundu <81185880+SKundu312@users.noreply.github.com>
2022-07-23 00:05:46 +0530

    Create Median2SortedArrays.cpp

---
+++ Searching and Sorting/Hard/Median2SortedArrays.cpp
@@ -0,0 +1,53 @@
+//O((m+n)log(m+n))
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+         int i=0,j=0,k=0;
+        vector<int>a;
+        while(i<nums1.size()&&j<nums2.size()){
+            if(nums1[i]<=nums2[j])
+                a.push_back(nums1[i++]);
+            else
+                a.push_back(nums2[j++]);               
+        }
+        while(i<nums1.size())
+            a.push_back(nums1[i++]);
+        while(j<nums2.size())
+             a.push_back(nums2[j++]);
+        
+         if(a.size()%2)
+            return a[a.size()/2];
+        else
+            return((double)(a[a.size()/2]+a[(a.size()/2)-1]))/2;
+       
+     
+    }
+
+//O(log(m+n))
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m=nums1.size(),n=nums2.size();
+         if(m>n)
+        return findMedianSortedArrays(nums2,nums1);//ensuring that binary search happens on minimum size array
+        
+    int low=0,high=m,medianPos=((m+n)+1)/2;
+    while(low<=high) {
+        int cut1 = (low+high)>>1;
+        int cut2 = medianPos - cut1;
+        
+        int l1 = (cut1 == 0)? INT_MIN:nums1[cut1-1];
+        int l2 = (cut2 == 0)? INT_MIN:nums2[cut2-1];
+        int r1 = (cut1 == m)? INT_MAX:nums1[cut1];
+        int r2 = (cut2 == n)? INT_MAX:nums2[cut2];
+        
+        if(l1<=r2 && l2<=r1) {
+            if((m+n)%2 != 0)
+                return max(l1,l2);
+            else 
+                return (max(l1,l2)+min(r1,r2))/2.0;
+        }
+        else if(l1>r2) high = cut1-1;
+        else low = cut1+1;
+    }
+    return 0.0;
+    }
+};
